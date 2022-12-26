a12d277570a6782743749b9ad2f6a6009689f99c
Markini <el19023@mail.ntua.gr>
2022-12-26 22:04:22 +0200

    minor changes in test_server.js

--- test_server.js
+++ test_server.js
@@ -6,6 +6,11 @@ app.get('/intelliq_api', (req, res) => {
   res.send('This is the api central page');
 });
 
+app.get('/error', (req, res) => {
+  res.status(404).send('Error 404 message');
+});
+
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
