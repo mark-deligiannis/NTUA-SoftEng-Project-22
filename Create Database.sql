ca7d42fadb70293bec82f7593ddb3cab3a12cdaa
Mark Deligiannis <el19023@mail.ntua.gr>
2023-01-21 14:06:59 +0200

    Finalized sql script for DB creation
    Done cooperatively using VS code live share.
    
    Co-Authored-By: Gerasimos Mountakis <115416431+ntua-el19007@users.noreply.github.com>
    Co-Authored-By: Petros Maratos <115232771+ntua-el19044@users.noreply.github.com>
    Co-Authored-By: ntua-el19017 <115298684+ntua-el19017@users.noreply.github.com>

---
+++ Create Database.sql
@@ -0,0 +1,65 @@
+-- ------------------------------------------
+-- Create database
+-- ------------------------------------------
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+DROP SCHEMA IF EXISTS IntelliQ;
+CREATE SCHEMA IntelliQ;
+USE IntelliQ;
+
+-- ------------------------------------------
+-- Create tables
+-- ------------------------------------------
+CREATE TABLE IF NOT EXISTS Questionnaire (
+    QuestionnaireID VARCHAR(127) PRIMARY KEY,
+    QuestionnaireTitle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Question (
+    QID VARCHAR(127) NOT NULL,
+    QuestionnaireID VARCHAR(127) NOT NULL,
+    Qtext VARCHAR(255) NOT NULL,
+    Required VARCHAR(10) NOT NULL,
+    Qtype VARCHAR(10) NOT NULL,
+    PRIMARY KEY (QID, QuestionnaireID),
+    FOREIGN KEY (QuestionnaireID)
+    REFERENCES Questionnaire(QuestionnaireID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Qoption (
+    OptID VARCHAR(127) NOT NULL,
+    QID VARCHAR(127) NOT NULL,
+    QuestionnaireID VARCHAR(127) NOT NULL,
+    Opttxt VARCHAR(255) NOT NULL,
+    NextQID VARCHAR(127) NOT NULL,
+    PRIMARY KEY (OptID, QID, QuestionnaireID),
+    FOREIGN KEY (QID,QuestionnaireID) REFERENCES Question(QID,QuestionnaireID),
+    FOREIGN KEY (NextQID) REFERENCES Question(QID)
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS Questionnaire_Keywords (
+	QuestionnaireID VARCHAR(127) NOT NULL,
+    Keyword_text VARCHAR(255) NOT NULL,
+    PRIMARY KEY (QuestionnaireID, Keyword_text),
+    FOREIGN KEY (QuestionnaireID)
+    REFERENCES Questionnaire(QuestionnaireID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Answer (
+	OptID VARCHAR(127) NOT NULL,
+    QID VARCHAR(127) NOT NULL,
+	QuestionnaireID VARCHAR(127) NOT NULL,
+	Session_ID CHAR(4) NOT NULL,
+	Answer_text VARCHAR(255) NULL,
+	PRIMARY KEY (OptID, QID, QuestionnaireID, Session_ID),
+	FOREIGN KEY (OptID , QID, QuestionnaireID)
+    REFERENCES Qoption (OptID , QID , QuestionnaireID)
+    ON DELETE CASCADE
+);
