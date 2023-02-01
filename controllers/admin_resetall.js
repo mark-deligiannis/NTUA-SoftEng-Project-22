112ddc17e3bea4085cd37e23a9d374040e2036c4
Mark Deligiannis <el19023@mail.ntua.gr>
2023-02-01 16:45:49 +0200

    Added resetall route
    That's it.

---
+++ controllers/admin_resetall.js
@@ -0,0 +1,29 @@
+const pool = require("../app.js");
+
+// handler for RESET
+async function resetall (req, res) {
+  var conn;
+  try {
+    // Get a connection from the pool
+    conn = await pool.getConnection();
+
+    // Delete all questionnaires, questions, options and answers.
+    // Due to cascading deletes we only need to do this with questionnaire.
+    await conn.query("DELETE FROM questionnaire;");
+
+    // Delete all keywords
+    await conn.query("DELETE FROM questionnaire_keywords;")
+
+    // Once everything is done set the status
+    res.status(200).send({"status":"OK"});
+  } catch (err) {
+    // Log the error message for debugging
+    console.log(`The following error occured:\n\n${err.message}\n`);
+    // Set the status to 500 (internal server error)
+    res.status(500).send({"status":"failed", "reason":err.message});
+  } finally {
+    if (conn) conn.end();
+  }
+}
+
+module.exports = resetall;
--- routes/admin.js
+++ routes/admin.js
@@ -9,5 +9,8 @@ router.post("/questionnaire_upd", upload.single('file'), admin_questionnaire_upd
 const admin_healthcheck = require('../controllers/admin_healthcheck');
 router.get('/healthcheck', admin_healthcheck);
 
+const admin_resetall = require('../controllers/admin_resetall');
+router.post('/resetall', admin_resetall);
+
 // Export router so that app.js can use it
 module.exports = router;
