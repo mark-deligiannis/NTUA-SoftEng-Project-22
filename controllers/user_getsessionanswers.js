5d04100d86c7db67f37b5f7875e84afa71972a5c
Mark Deligiannis <el19023@mail.ntua.gr>
2023-02-02 01:32:20 +0200

    Create user_getsessionanswers.js

---
+++ controllers/user_getsessionanswers.js
@@ -0,0 +1,60 @@
+const pool = require("../app.js");
+
+async function user_getsessionanswers_handler(req, res) {
+    const questionnaireID = req.params.questionnaireID;
+    const sessionID = req.params.session;
+
+    var json = {
+        questionnaireID: questionnaireID,
+        session: sessionID,
+        answers: []
+    };
+
+    let conn;
+    try {
+        // Get a connection from the pool
+        conn = await pool.getConnection();
+
+        // Create entry in questionnaire
+        // Prepare the statement
+        var stmt = await conn.prepare(
+            `(select ans.QID as qID, ans.Answer_text as ans
+            from answer ans, qoption opt 
+            where ans.OptID = opt.OptID 
+            and ans.QID = opt.QID
+            and ans.QuestionnaireID = opt.QuestionnaireID
+            and (ans.QuestionnaireID, ans.Session_ID) == (?, ?),
+            and opt.Opttxt = '<open string>')
+            union
+            (select ans.QID as ans, opt.OptID as ans
+            from answer ans, qoption opt
+            where ans.OptID = opt.OptID
+            and ans.QID = opt.QID
+            and ans.QuestionnaireID = opt.QuestionnaireID
+            and (ans.QuestionnaireID, ans.Session_ID) == (?, ?),
+            and opt.Opttxt != '<open string>')`
+        );
+        // Execute the statement
+        json["answers"] = await stmt.execute([questionnaireID, sessionID, questionnaireID, sessionID]);
+        
+        // Get rid of metadata returned by mariadb
+        delete json["answers"].meta;
+
+        if (json["answers"][0]) {
+            res.status(200).send(json);
+        }
+        else {
+            res.status(402).send({});
+        }
+    } catch (err) {
+        // Log the error message for debugging
+        console.log(`The following error occured:\n\n${err.message}\n`);
+
+        // Set the status to 500 (internal server error)
+        res.status(500).sendFile(path.join(__dirname,"../templates/error_500.html"));
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = user_getsessionanswers_handler;
