eb63aef23f1b1fbf964337b0e6d58b02886422eb
Mark Deligiannis <el19023@mail.ntua.gr>
2023-02-01 23:52:12 +0200

    Added user_questionnaire & user_questionnaire_dump
    - User_questionnaire fetches a questionnaire (no options)
    - User_questionnaire (optional) fetches the entire questionnaire, as it was inserted (with options)

---
+++ controllers/user_questionnaire.js
@@ -0,0 +1,61 @@
+const pool = require("../app.js");
+
+async function user_questionnaire(req, res) {
+    // Get questionnaireID from request
+    var questionnaireID = req.params.questionnaireID;
+    // Declare response json
+    var json = {};
+    let conn;
+    try {
+        // Connect to database
+        conn = await pool.getConnection();
+
+        // Search for the questionnaire
+        // Prepare the statement
+        var stmt = await conn.prepare("SELECT * FROM questionnaire WHERE questionnaireID=?;");
+        // Execute the statement
+        var query_response = await stmt.execute([questionnaireID]);
+
+        if (query_response[0]) {
+            // Set main properties of questionnaire            
+            json["questionnaireID"] = query_response[0]["QuestionnaireID"];
+            json["questionnaireTitle"] = query_response[0]["QuestionnaireTitle"];
+
+            // Get the relevant keywords
+            stmt = await conn.prepare("SELECT Keyword_text FROM questionnaire_keywords WHERE questionnaireID=?;");
+            query_response = await stmt.execute([questionnaireID]);
+
+            // Set the keywords in the json (format them accordingly)
+            json["keywords"] = query_response.map(x => x['Keyword_text']);
+
+            // Get all the relevant questions
+            stmt = await conn.prepare("SELECT * FROM Question WHERE questionnaireID=? AND QID!='-' ORDER BY QID ASC;");
+            query_response = await stmt.execute([questionnaireID]);
+
+            json["questions"] = [];
+            jq = json["questions"];
+            // fill json["questions"]
+            query_response.forEach((question, index) => {
+                jq[index] = {
+                    qID: question["QID"],
+                    qtext: question["Qtext"],
+                    required: question["Required"],
+                    type: question["Qtype"]
+                }
+            });
+
+            res.status(200).send(json);
+        }
+        else {
+            res.status(402).send({});
+        }
+    } catch (err) {
+        // Log the error message for debugging
+        console.log(`The following error occured:\n\n${err.message}\n`);
+        res.status(500).send({});
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = user_questionnaire;
