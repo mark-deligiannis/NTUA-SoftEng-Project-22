d74549df3c93af2bfa556bb8de271948d29d0ae8
ntua-el19050 <el19050@mail.ntua.gr>
2023-02-08 18:48:00 +0200

    Changes to App and creeation of basic Component fi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Admin from "./Components/Admin";
 
 function App() {
   return (
@@ -9,8 +12,18 @@ function App() {
       <h2>Our questions, your answers</h2>
       <h2>Log in:</h2>
       <div class="buttons">
-        <button class="button" onclick="window.location.href='/Components/Admin'">Administrator</button>
-        <button class="button" onclick="window.location.href='/Components/User'">User</button>
+      <Link to={"/Admin"}>
+                  Admin
+                </Link>
+        {/*<button class="button" onclick="window.location.href='/Admin'">Administrator</button>
+        <button class="button" onclick="window.location.href='/User'">User</button> */}
+      </div>
+      <div>
+        <Router>
+          <Routes>
+            <Route exact path={ "/Admin"} component={Admin} />
+          </Routes>
+          </Router>
       </div>
     </div>
   );
