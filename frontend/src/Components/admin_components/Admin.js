7004f62f906c0d997c454b8ecc5bbd8284d8d471
ntua-el19050 <el19050@mail.ntua.gr>
2023-02-09 14:57:35 +0200

    WorkinG Download JSON/CSV, encoding issue with CSV . > > Co-authored-by: ntua-el19017 <el19017@mail.ntua.gr>

--- frontend/src/Components/admin_components/Admin.js
+++ frontend/src/Components/admin_components/Admin.js
@@ -1,12 +1,14 @@
 import React from "react";
 const host = "localhost"
 const port = 9103
-const Reset_URL = `http://${host}:${port}/intelliq_api/admin/resetall`;
-const API_URL = `http://${host}:${port}/intelliq_api/admin/questionnaire_upd`;
+const id = "QQ000"
+const ResetAll_URL = `http://${host}:${port}/intelliq_api/admin/resetall`;
+const Upload_URL = `http://${host}:${port}/intelliq_api/admin/questionnaire_upd`;
+const DownloadCSV_URL = `http://${host}:${port}/intelliq_api/questionnaire/${id}?format=csv`;
+const DownloadJSON_URL = `http://${host}:${port}/intelliq_api/questionnaire/${id}`;
 
 
 class Admin extends React.Component {
-  //API_URL = "localhost:9103/intelliq_api/admin/questionnaire_upd";
   
   constructor(props) {
     super(props);
@@ -29,7 +31,7 @@ class Admin extends React.Component {
 
  
 
-    fetch(API_URL, {
+    fetch(Upload_URL, {
       method: "POST",
       mode: 'no-cors',
       body: formData,
@@ -41,7 +43,7 @@ class Admin extends React.Component {
   
   handleDelete() {
 
-    fetch(Reset_URL, {
+    fetch(ResetAll_URL, {
       method: 'POST',
       mode: 'no-cors',
       
@@ -52,6 +54,58 @@ class Admin extends React.Component {
       
   };
 
+  exportCSV() {
+
+  /*fetch(DownloadCSV_URL)
+  .then(response => {
+    response.blob({ type: "text/csv;charset=UTF-8" }).then(blob => {
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = "data.csv";
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
+  })
+  .catch(error => console.error(error));*/
+  fetch(DownloadCSV_URL)
+  .then(response => {
+    return response.text();
+  })
+  .then(text => {
+    console.log(text);
+    var csv = new Blob([text], { type: 'text/csv;charset=utf-8' });
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(csv);
+    link.download = 'data.csv';
+    link.click();
+  })
+  .catch(error => {
+    console.error(error);
+  
+  });
+  };
+
+  exportJSON() {
+
+    fetch(DownloadJSON_URL)
+    .then(response => {
+      response.blob().then(blob => {
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = "data.json";
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      });
+    })
+    .catch(error => console.error(error));
+    };
+
   render() {
     return (
       <div>
@@ -60,6 +114,8 @@ class Admin extends React.Component {
           <button type="submit">Upload</button>
         </form>
         <button onClick={() => this.handleDelete()}>Delete All</button>
+        <button onClick={this.exportCSV}>Export CSV</button>
+        <button onClick={this.exportJSON}>Export JSON</button>
       </div>
     );
   }
