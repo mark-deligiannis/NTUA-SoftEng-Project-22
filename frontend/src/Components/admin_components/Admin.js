7bb63cd7fe312cda75c6b3623c37f4e9bbb95390
Theodoros <el19017@mail.ntua.gr>
2023-02-13 20:05:29 +0200

    Changed class components to functional components

--- frontend/src/Components/admin_components/Admin.js
+++ frontend/src/Components/admin_components/Admin.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import './Admin.css'
 import { Link } from 'react-router-dom';
 const host = "localhost"
@@ -9,25 +9,16 @@ const Upload_URL = `http://${host}:${port}/intelliq_api/admin/questionnaire_upd`
 var id = "QQ000";
 
 
-class Admin extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedFile: null,
-
-    };
-  }
-
-  onFileChange = (event) => {
-    this.setState({ selectedFile: event.target.files[0] });
-  };
-
-  onFileUpload = (event) => {
+function Admin()  {
+  const [selectedFile, setselectedFile] = useState(null)
+  
+  
+  const onFileChange = (event) => {setselectedFile(event.target.files[0]) }
+ 
+  const  onFileUpload = (event) => {
     event.preventDefault();
 
-    const { selectedFile } = this.state;
+   // const { selectedFile } = this.state;
     const formData = new FormData();
     formData.append("file", selectedFile);
 
@@ -39,9 +30,10 @@ class Admin extends React.Component {
         "Content-Type": "multipart/form-data; boundary=${formData._boundary}",
       },
     })
-  };
+  }
   
-  handleDelete() {
+  const handleDelete=()=>{
+  
     fetch(ResetAll_URL, {
       method: 'POST',
       mode: 'no-cors',
@@ -49,31 +41,27 @@ class Admin extends React.Component {
         "Content-Type": "application/w-xxx-form-urlencoded",
       },
     })     
-  };
+  }
 
   
 
-  render() {
+ 
 
     return (
       <div>
-        <form method="post" encType="multipart/form-data" onSubmit={this.onFileUpload} >
-          <label htmlFor="file-input">
-            <span className="custom-file-upload">Choose File</span>
-          </label>
-          <input type="file" id="file-input" onChange={this.onFileChange} style={{ display: "none" }} />
-          <button className="submit" type="submit">Upload</button>
-        </form>
-        <button className="delete" onClick={() => this.handleDelete()}>Delete All</button>
-        <br/>
-        <Link to={`/Admin/Questionnaires`}> <button className="nextPage" >Questionnaires</button></Link>
-        
-          
-        
-
-      </div>
+      <form method="post" encType="multipart/form-data" onSubmit={onFileUpload} >
+        <label htmlFor="file-input">
+          <span className="custom-file-upload">Choose File</span>
+        </label>
+        <input type="file" id="file-input" onChange={onFileChange} style={{ display: "none" }} />
+        <button className="submit" type="submit">Upload</button>
+      </form>
+      <button className="delete" onClick={handleDelete}>Delete All</button>
+      <br/>
+      <Link to={`/Admin/Questionnaires`}> <button className="nextPage" >Questionnaires</button></Link>
+    </div>
     );
-  }
+  
 }
 
 export default Admin;
