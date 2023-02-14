40af3a50b915a93ca853e9b13ea05c9a09655f52
Theodoros <el19017@mail.ntua.gr>
2023-02-14 07:19:46 +0200

    Change from Class Components to Function Components
    All functions implemented,the only issue is the use of promises

--- frontend/src/Components/admin_components/Admin_graphs.js
+++ frontend/src/Components/admin_components/Admin_graphs.js
@@ -1,35 +1,20 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 import { Pie } from "react-chartjs-2";
+import { useParams, useNavigate } from 'react-router-dom';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import "./Admin_graphs.css"
-const host = "localhost"
-const port = 9103
-var id = "QQ001";
-
-
+import "./Admin_graphs.css";
+const host = "localhost";
+const port = 9103;
+var id = 'QQ001';
+const buttons = [];
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const options = {
   maintainAspectRatio: false,
   responsive: true,
-  width: 350,
-  height: 350,
-  textColor: '#000000',
-  plugins: {
-    legend: {
-      labels: {
-        color: '#000000',
-        boxWidth: 15,
-        padding: 10,
-        fontStyle: 'bold',
-        useBorderRadius: true,
-        borderRadius: '20px',
-        usePointStyle: true
-      }
-    }
-  }
-
+  width: 100,
+  height: 100
 };
 
 var getPieChartData = (data) => {
@@ -66,129 +51,210 @@ var getPieChartData = (data) => {
   };
 };
 
-export default class AdminGraphs extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.getAnswersToQuestions = this.getAnswersToQuestions.bind(this);
-        this.toggleDisplayAnswers = this.toggleDisplayAnswers.bind(this);
-    
-        this.state = {
-            answers: [],
-            displayAnswers: new Array(4)
-        };
-      }
-    
-      toggleDisplayAnswers(i) {
-        this.state.displayAnswers[i] = !this.state.displayAnswers[i];
-        this.setState({  displayAnswers: this.state.displayAnswers });
-      };
-  
-      async getAnswersToQuestions(qid) {
-       
-        //const qid = this.state.qid;
-        const Answers_URL = `http://${host}:${port}/intelliq_api/getquestionanswers/${id}/${qid}`;
-    
-       
-        const response = await fetch(Answers_URL, {
-          method: 'GET'
-        }).then(res => res.json())
-        .then(data => {
-          if (data) {
-            this.setState(prevState => {
-            const answers = { ...prevState.answers };
-            answers[`${qid}`] = data.answers;
-             return{ answers }});
-          }
-        })
-          .catch(error => {console.error("Error",error);
-        });
-        
-        };
-  
-        componentDidMount() {
-          for (var i=0; i<4; i++){
-            this.state.displayAnswers[i] = false;
-            
-          }
-          for (var i=1; i<5; i++){
-            this.getAnswersToQuestions(`Q0${i}`);
-          }
-          
-        }
-        
-  
-        graphs(qid) {
-  
-          const data = new Array(this.state.answers[qid].length)    // a new array with the size (rows) of reply array of objects size
-          for (var i=0; i<this.state.answers[qid].length; i++) data[i] = new Array(2);  // columns of it
-          for (i=0; i<this.state.answers[qid].length; i++) {
-            data[i][0] = this.state.answers[qid][i].ans;
-            data[i][1] = this.state.answers[qid][i].session;
-          }
-          var graphAns = {};
-        data.forEach((item) => {
-          graphAns[item[0]] = (graphAns[item[0]] || 0) + 1;
-        });
-      
-        graphAns= Object.entries(graphAns);
-  
-      
-          return (
-            <>
-              
-              {graphAns && (
-              <>
-                {<Pie data={getPieChartData(graphAns)} options={options} />}
-              </>)}
-            </>
-          );
-  
-        };
 
-    render() {
-      const buttons = [];
-    for (let i = 0; i < 4; i++) {
+
+
+
+
+async function getAnswersToQuestions(questid,qid) {
+  const Answers_URL = `http://localhost:9103/intelliq_api/getquestionanswers/${questid}/${qid}`;
+  
+  try {
+    const res = await fetch(Answers_URL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });try{
+    const data = await res.json();
+    if (data) {
+      console.log(6,data)
+      var z={};
+      data===null ? z[`${qid}`]=[{}]: z[`${qid}`]=data.answers;
+      console.log(6,data)
+    return z;}
+  }
+    catch(error){
+      var z={};
+      z[`${qid}`]=[-1];
+      return z;
+    }
+    
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+async function getQuestionnaire(quest) {
+  const Questionnaire_URL = `http://${host}:${port}/intelliq_api/questionnairedump/${quest}`;
+    
+  try {
+    const res = await fetch(Questionnaire_URL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    const data = await res.json();
+    if (data) {
+      return data.questions;
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+async function getcount(quest) {
+  const Count_URL = `http://${host}:${port}/intelliq_api/questionnaireanscount/${quest}`;
+    
+  try {
+    const res = await fetch(Count_URL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    const data = await res.json();
+    if (data) {
+      console.log(data)
+      return data.Number;
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+function initialiseDisplayAnswers(lim) {
+  return Array.from({ length: lim }, () => false);
+}
+
+async function getAllAnswers(questid,question) {
+  const promises = [];
+  for (let i = 0; i < question[0].length; i++) {
+    promises.push(getAnswersToQuestions(questid,question[0][i].qID));
+  }
+  return Promise.all(promises);
+}
+
+
+async function getQuestions(questid) {
+  const promises=[];
+ 
+  promises.push(getQuestionnaire(questid));
+  
+  return Promise.all(promises);
+}
+async function getpcount(questid) {
+  const promises3=[];
+ 
+  promises3.push(getcount(questid));
+  
+  return Promise.all(promises3);
+}
+
+async function start(quest) {
+  try {
+    
+    const questionnaire = await getQuestions(quest);
+    console.log(7,questionnaire[0].length)
+    const allAnswers = await getAllAnswers(quest,questionnaire);
+    const count=await getpcount(quest);
+    const displayAnswers = initialiseDisplayAnswers(questionnaire[0].length);
+    console.log('Questionnaire:', questionnaire);
+    console.log('All Answers:', allAnswers);
+    console.log('Display Answers:', displayAnswers);
+    return {c:count,q:questionnaire,a:allAnswers,d:displayAnswers};
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+export default function AdminGraphs() {
+  const params = useParams()
+  const [count,setCount]=useState(0)
+  const [questionnaire,setQuestionnaire]=useState([])
+  const [allAnswers,setAllAnswers]=useState([])
+  const [displayAnswers,setDisplayAnswers]=useState([])
+  /*const [graphAns,setGraphAns]=useState([])
+  const  graphs=(qid)=> {
+  
+    const data = new Array(allAnswers[qid].length)    // a new array with the size (rows) of reply array of objects size
+    for (var i=0; i<allAnswers[qid].length; i++) data[i] = new Array(2);  // columns of it
+    for (i=0; i<allAnswers[qid].length; i++) {
+      data[i][0] = allAnswers[qid][i].ans;
+      data[i][1] = allAnswers[qid][i].session;
+    }
+    var graphAns1 = {};
+  data.forEach((item) => {
+    graphAns1[item[0]] = (graphAns1[item[0]] || 0) + 1;
+  });
+  
+   setGraphAns( Object.entries(graphAns1));
+}
+  
+  
+  
+  
+   function toggleDisplayAnswers(i){
+    var temp=displayAnswers
+    temp[i]=!displayAnswers[i]
+    setDisplayAnswers(temp);
+    return;
+  }
+*/
+  useEffect(() => {
+    var z=start(params.id);
+    setQuestionnaire(z.q);
+    setCount(z.c);
+    setAllAnswers(z.a);
+    setDisplayAnswers(z.d)
+    console.log(count,questionnaire,allAnswers,displayAnswers)
+    
+
+    /*for (let i = 0; i < questionnaire.length; i++) {
       buttons.push(
-        <div className="col-md-6">
-          <div className="text-center">
-          <h1>Question</h1>
+        <div className="display-container">
         <button
           key={i}
           className="button"
           onClick={() => {
-            this.toggleDisplayAnswers(i);
+            
+          toggleDisplayAnswers(i);
           }}
         >
          Question {i+1}
         </button>
-        </div>
         <div className="pie-container">
         {
-        this.state.displayAnswers[i] && (
-            <div key={i+1} className="chart-container">
-              {this.graphs(`Q0${i+1}`)}  
+        displayAnswers[i] && (
+            <div key={i+1}>
+              {graphs(`Q0${i+1}`)}  
             </div>
           )
         }
         </div>
         </div>
-      );
-    }
+      );*/
+     
 
-    return (
-      <div className="container">
-      <div className="row">
-      {buttons}
-      </div> 
-      
-      <br/>
+  }, []);
 
-      <div className="buttons">
-        <Link to={`/Admin/Questionnaires`}> <button className="button" >Questionnaire</button></Link>
-        <Link to={"/Admin"}> <button className="button" >Back</button></Link>
-      </div>
-      </div>
-  );
-}
-}
+  const handleClick = () => {
+    setCount(count + 1);}
+
+  
+  
+  
+  return (
+    <div className="welcome">
+          <p> Total answers: { count } </p>
+          <button onClick={handleClick}>Increment</button>
+          </div>
+  )
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  };
