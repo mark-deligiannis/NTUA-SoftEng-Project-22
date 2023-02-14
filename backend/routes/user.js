7320764d83607964977988c10e631d60b7918f1c
Mark Deligiannis <el19023@mail.ntua.gr>
2023-02-14 18:40:37 +0200

    Added API functional testing & Fixed backend bug
    - backend server crashed when posting to admin/questionnaire_upd with no files

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,6 @@ router.use(BodyParser.urlencoded({extended: true}))
 
 const user_question_handler = require("../controllers/user_question.js")
 const user_questionnaire_handler = require("../controllers/user_questionnaire.js")
-const user_questionnaire_dump_handler = require("../controllers/user_questionnaire_dump.js")
 const user_getquestionanswers_handler = require("../controllers/user_getquestionanswers.js")
 const user_getsessionanswers_handler = require("../controllers/user_getsessionanswers.js")
 const user_doanswer_handler = require("../controllers/user_doanswer.js")
@@ -15,7 +14,6 @@ const user_fetchkeywords = require("../controllers/user_fetchkeywords.js")
 const user_questionnaireanscount = require("../controllers/user_questionnaireanscount.js")
 
 router.get(`/questionnaire/:questionnaireID`, user_questionnaire_handler);
-router.get(`/questionnairedump/:questionnaireID`, user_questionnaire_dump_handler);
 router.get(`/question/:questionnaireID/:questionID`, user_question_handler);
 router.post(`/doanswer/:questionnaireID/:questionID/:session/:optionID`, user_doanswer_handler);
 router.get(`/getsessionanswers/:questionnaireID/:session`, user_getsessionanswers_handler);
