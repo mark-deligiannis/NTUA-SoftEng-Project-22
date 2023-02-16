ad20ae0b02dc19c442d2759f17bb41dc1e104efe
Gerasimos Mountakis <el19007@mail.ntua.gr>
2023-02-16 19:09:14 +0200

    Some more formatting

--- frontend/src/Components/user_components/User.js
+++ frontend/src/Components/user_components/User.js
@@ -8,161 +8,161 @@ const KEYS_URL = "http://localhost:9103/intelliq_api/fetchkeywords";
 
 function User() {
 
-    // Store all keywords contained in the database
-    const [keywords, setKeywords] = useState([])
+  // Store all keywords contained in the database
+  const [keywords, setKeywords] = useState([])
 
-    // Store all selected keywords
-    const [selectedOptions, setSelectedOptions] = useState([])
+  // Store all selected keywords
+  const [selectedOptions, setSelectedOptions] = useState([])
 
-    // Store all fetched questionnaires
-    const [questionnaires, setQuestionnaires] = useState([])
+  // Store all fetched questionnaires
+  const [questionnaires, setQuestionnaires] = useState([])
 
 
-    // Fetch all available questionnaires and all available keywords from the database 
-    useEffect(() => {
-        
-        var requestOptions = {
-			method: 'POST',
-			mode: 'cors',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		}
-
-		fetch(API_URL, requestOptions)
-			.then(res => res.json())
-       		.then(data => setQuestionnaires(data))
-            .catch(error => {console.error("Error",error)});
-
-		requestOptions = {
-			method: 'GET',
-			mode: 'cors',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		}
-	
-		fetch(KEYS_URL, requestOptions)
-			.then(res => res.json())
-			.then(data => setKeywords(data.Keywords))
-            .catch(error => {console.error("Error",error)});
-
-    }, [])
-
-   
-    // Creating the x-www-form-urlencoded post request to filter displayed questionnaires based on user selected keywords
-    useEffect(() => {
-       
-        // if keywords are selected
-    	if (selectedOptions.length) {
-            let payload = ''
-
-            // forming the x-www-form-urlencoded body of the post request
-            for (var i = 0; i < selectedOptions.length; i++) { 
-                payload += `keywords[${i}]=${selectedOptions[i].value}`
-                if (i < selectedOptions.length-1) {
-                    payload += '&'
-                }
-            }
-            
-            var requestOptions = {
-                method: 'POST',
-                mode: 'cors',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: payload
-            }
-            
-
-            fetch(API_URL, requestOptions)
-                .then(res => res.json())
-                .then(response =>  setQuestionnaires(response))
-                .catch(error => {console.error("Error",error)});
-        }
-      else { // no keyword seleced, fetch all questionnaires
-        var requestOptions = {
-			method: 'POST',
-			mode: 'cors',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		}
-
-		fetch(API_URL, requestOptions)
-			.then(res => res.json())
-       		.then(data => setQuestionnaires(data))
-            .catch(error => {console.error("Error",error)});
+  // Fetch all available questionnaires and all available keywords from the database 
+  useEffect(() => {
+    
+    var requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       }
-            
-	}, [selectedOptions]) // method is triggered each time selectedOptions changes
+
+    fetch(API_URL, requestOptions)
+      .then(res => res.json())
+      .then(data => setQuestionnaires(data))
+      .catch(error => {console.error("Error",error)});
+
+    requestOptions = {
+      method: 'GET',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }
+
+    fetch(KEYS_URL, requestOptions)
+      .then(res => res.json())
+      .then(data => setKeywords(data.Keywords))
+      .catch(error => {console.error("Error",error)});
+
+  }, [])
 
 
+  // Creating the x-www-form-urlencoded post request to filter displayed questionnaires based on user selected keywords
+  useEffect(() => {
+    
+    // if keywords are selected
+    if (selectedOptions.length) {
+      let payload = ''
 
-
-    var table = () => {
-        
-            const data = new Array(questionnaires.length)    // a new array with the size (rows) of reply array of objects size
-            for (var i=0; i<questionnaires.length; i++) data[i] = new Array(2);  // columns of it
-            for (i=0; i<questionnaires.length; i++) {
-                data[i][0] = questionnaires[i].QuestionnaireID;
-                data[i][1] = questionnaires[i].QuestionnaireTitle;
-            }
-            return (
-                <div id="table-responsive">
-                    <table>
-                        <thead id="questionnaire">
-                            <tr>
-                                <th><h3><b>Questionnaire ID</b></h3></th>
-                                <th><h3><b>Title</b></h3></th>
-                                <th><h3><b>Answer Questionnaire</b></h3></th>
-                            </tr>
-                        </thead>
-                        <tbody id="questionnaire">
-                            {data.slice(0, data.length).map((item, index) => {
-                                return (
-                                    <tr>
-                                        <td><h5>{item[0]}</h5></td>
-                                        <td><h5>{item[1]}</h5></td>
-                                        <td><Link to={`Answer/${item[0]}`}> <button className="button" >Answer</button></Link></td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            );
+      // forming the x-www-form-urlencoded body of the post request
+      for (var i = 0; i < selectedOptions.length; i++) { 
+        payload += `keywords[${i}]=${selectedOptions[i].value}`
+        if (i < selectedOptions.length-1) {
+          payload += '&'
         }
+      }
+      
+      var requestOptions = {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: payload
+      }
+      
 
+      fetch(API_URL, requestOptions)
+        .then(res => res.json())
+        .then(response =>  setQuestionnaires(response))
+        .catch(error => {console.error("Error",error)});
+    }
+    else { // no keyword seleced, fetch all questionnaires
+      var requestOptions = {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }
 
+      fetch(API_URL, requestOptions)
+        .then(res => res.json())
+        .then(data => setQuestionnaires(data))
+        .catch(error => {console.error("Error",error)});
+    }
         
-        return (
-                <div className="container">
-                    <div className="top">
-                    <center>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td className="admintd"><h2> Select Questionnaires </h2></td>
-                                    <td><Link to={"/"}> <button className="button" >Homepage</button></Link></td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="2" className="admintd"><h3> View all available questionnaires or filter by keywords </h3></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </center>
-                    <div className="app">
-                        <center><h2>Choose keywords</h2></center>
-                        <div className="dropdown-container">
-                            <Select
-                                options={keywords.map(item => ({value: item, label: item}))}
-                                placeholder="Select Keywords"
-                                value={selectedOptions}
-                                onChange={data => setSelectedOptions(data)}
-                                isSearchable={true}
-                                isMulti
-                            />
-                        </div>
-                    </div>
-                    </div>
-                    <header className="jumbotron" id="questionnaires">
-                        {table()}
-                    </header>
-                </div>
-        )
+  }, [selectedOptions]) // method is triggered each time selectedOptions changes
+
+
+
+
+  var table = () => {
+    
+    const data = new Array(questionnaires.length)    // a new array with the size (rows) of reply array of objects size
+    for (var i=0; i<questionnaires.length; i++) data[i] = new Array(2);  // columns of it
+    for (i=0; i<questionnaires.length; i++) {
+      data[i][0] = questionnaires[i].QuestionnaireID;
+      data[i][1] = questionnaires[i].QuestionnaireTitle;
+    }
+    return (
+      <div id="table-responsive">
+        <table>
+          <thead id="questionnaire">
+            <tr>
+              <th><h3><b>Questionnaire ID</b></h3></th>
+              <th><h3><b>Title</b></h3></th>
+              <th><h3><b>Answer Questionnaire</b></h3></th>
+            </tr>
+          </thead>
+          <tbody id="questionnaire">
+            {data.slice(0, data.length).map((item, index) => {
+                return (
+                    <tr>
+                        <td><h5>{item[0]}</h5></td>
+                        <td><h5>{item[1]}</h5></td>
+                        <td><Link to={`Answer/${item[0]}`}> <button className="button" >Answer</button></Link></td>
+                    </tr>
+                );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+
+    
+  return (
+    <div className="container">
+      <div className="top">
+        <center>
+          <table>
+            <tbody>
+              <tr>
+                <td className="admintd"><h2> Select Questionnaires </h2></td>
+                <td><Link to={"/"}> <button className="button" >Homepage</button></Link></td>
+              </tr>
+              <tr>
+                <td colSpan="2" className="admintd"><h3> View all available questionnaires or filter by keywords </h3></td>
+              </tr>
+            </tbody>
+          </table>
+        </center>
+        <div className="app">
+          <center><h2>Choose keywords</h2></center>
+          <div className="dropdown-container">
+              <Select
+                  options={keywords.map(item => ({value: item, label: item}))}
+                  placeholder="Select Keywords"
+                  value={selectedOptions}
+                  onChange={data => setSelectedOptions(data)}
+                  isSearchable={true}
+                  isMulti
+              />
+          </div>
+        </div>
+      </div>
+      <header className="jumbotron" id="questionnaires">
+        {table()}
+      </header>
+    </div>
+  )
 }
 
 export default User;
--- frontend/src/Components/user_components/View_session.js
+++ frontend/src/Components/user_components/View_session.js
@@ -5,151 +5,150 @@ const SESSION_URL = "http://localhost:9103/intelliq_api/getsessionanswers/";
 const QUESTION_URL = "http://localhost:9103/intelliq_api/question/";
 
 function ViewSession() {
-    // Store all answers retrieved from API
-    const [state, setState] = useState({
-        ans: [],
-        index: -1
-    })
+  // Store all answers retrieved from API
+  const [state, setState] = useState({
+    ans: [],
+    index: -1
+  })
 
-    // Store each question to match IDs with text
-    const [question, setQuestion] = useState([
-        {
-            qID: '',
-            qtext: '',
-            options: []
-        }
-    ]);
+  // Store each question to match IDs with text
+  const [question, setQuestion] = useState([
+    {
+      qID: '',
+      qtext: '',
+      options: []
+    }
+  ]);
 
-    // Store final answers to be displayed
-    const [answer,setAnswer] = useState([]);
+  // Store final answers to be displayed
+  const [answer,setAnswer] = useState([]);
 
-    // Match the parameters (id, session) from the url
-    const params = useParams()
+  // Match the parameters (id, session) from the url
+  const params = useParams()
 
-    // Fetch answers for the specific questionnaire and session using the API
-    // Only runs when component mounts
-    useEffect(() => {
+  // Fetch answers for the specific questionnaire and session using the API
+  // Only runs when component mounts
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    }
+      
+    // Convert the fetched answers to JSON and the function state
+    fetch(SESSION_URL + params.id + '/' + params.session, requestOptions)
+      .then(res => res.json())
+      .then(data => setState({ans: data.answers, index: 0}))
+  }, [])
+
+  
+  // Fetch the i-th question answered (position indicated by state.index)
+  // Only runs when state is updated
+  useEffect(() => {
+    // Only run after the first useEffect() fetches all answers
+    if (state.index >= 0) {
+      if(state.index < state.ans.length) {
+        var quest = state.ans[state.index].qID
+
         const requestOptions = {
             method: 'GET',
             mode: 'cors',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          }
+        }
           
-          // Convert the fetched answers to JSON and the function state
-          fetch(SESSION_URL + params.id + '/' + params.session, requestOptions)
-            .then(res => res.json())
-            .then(data => setState({ans: data.answers, index: 0}))
-    }, [])
+        // Save only the info we need (question title, ID and available options)
+        fetch(QUESTION_URL + params.id + '/' + quest, requestOptions)
+          .then(res => res.json())
+          .then(data => setQuestion({
+              qID: data.qID,
+              qtext: data.qtext,
+              options: data.options
+              }))
+      }
+    }
+  }, [state])
 
-    
-    // Fetch the i-th question answered (position indicated by state.index)
-    // Only runs when state is updated
-    useEffect(() => {
-        // Only run after the first useEffect() fetches all answers
-        if (state.index >= 0) {
-            if(state.index < state.ans.length) {
-                var quest = state.ans[state.index].qID
-
-                const requestOptions = {
-                    method: 'GET',
-                    mode: 'cors',
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                }
-                
-                // Save only the info we need (question title, ID and available options)
-                fetch(QUESTION_URL + params.id + '/' + quest, requestOptions)
-                    .then(res => res.json())
-                    .then(data => setQuestion({
-                        qID: data.qID,
-                        qtext: data.qtext,
-                        options: data.options
-                        }))
+  // Build the final answer to be displayed
+  // Only runs when question is updated
+  useEffect(() => {
+    // Only run after the first useEffect() fetches all answers
+    if (state.index >= 0) {
+      // If answer originated from text field push question title with the original answer
+      if (question.options[0].opttxt === "<open string>") {
+        setAnswer([
+            ...answer,
+            {
+            qtext: question.qtext,
+            ansTXT: state.ans[state.index].ans
             }
+        ]);
+      }
+      // Else push question title with selected option text
+      else {
+        for (var j=0; j<question.options.length; j++) {
+          if (question.options[j].optID === state.ans[state.index].ans) {
+            setAnswer([
+              ...answer,
+              {
+                qtext: question.qtext,
+                ansTXT: question.options[j].opttxt
+              }
+            ])
+          }
         }
-    }, [state])
-
-    // Build the final answer to be displayed
-    // Only runs when question is updated
-    useEffect(() => {
-        // Only run after the first useEffect() fetches all answers
-        if (state.index >= 0) {
-            // If answer originated from text field push question title with the original answer
-            if (question.options[0].opttxt === "<open string>") {
-                setAnswer([
-                    ...answer,
-                    {
-                    qtext: question.qtext,
-                    ansTXT: state.ans[state.index].ans
-                    }
-                ]);
-            }
-            // Else push question title with selected option text
-            else {
-                for (var j=0; j<question.options.length; j++) {
-                    if (question.options[j].optID === state.ans[state.index].ans) {
-                        setAnswer([
-                            ...answer,
-                            {
-                                qtext: question.qtext,
-                                ansTXT: question.options[j].opttxt
-                            }
-                        ])
-                    }
-                }
-            }
-        }
-        var s = state.ans
-        setState({ans: s, index: state.index+1})
-    }, [question])
+      }
+    }
+    var s = state.ans
+    setState({ans: s, index: state.index+1})
+  }, [question])
 
 
-    // Build table with final answers
-    const table = () => {
-        
-        const data = new Array(answer.length)    // a new array with the size (rows) of reply array of objects size
-        for (var i=0; i<answer.length; i++) data[i] = new Array(2);  // columns of it
-        for (i=0; i<answer.length; i++) {
-            
-            data[i][0] = answer[i].qtext;
-            data[i][1] = answer[i].ansTXT;
-        }
-        return (
-            <div id="table-responsive">
-                <table>
-                    <thead id="session">
-                        <tr>
-                            <th><h3><b>Question</b></h3></th>
-                            <th><h3><b>Answer</b></h3></th>
-                        </tr>
-                    </thead>
-                    <tbody id="session">
-                        {data.slice(0, data.length).map((item, index) => {
-                            return (
-                                <tr>
-                                    <td><h5>{item[0]}</h5></td>
-                                    <td><h5>{item[1]}</h5></td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-            );
-        }
-
+  // Build table with final answers
+  const table = () => {
+      
+    const data = new Array(answer.length)    // a new array with the size (rows) of reply array of objects size
+    for (var i=0; i<answer.length; i++) data[i] = new Array(2);  // columns of it
+    for (i=0; i<answer.length; i++) {
+      data[i][0] = answer[i].qtext;
+      data[i][1] = answer[i].ansTXT;
+    }
     return (
-        <div className="container"> 
-            <div>
-                <h2> Your Answers </h2>
-            </div>
-            <header className="jumbotron" id="session">
-                {table()}
-            </header>
-            <div className="buttons">
-                <Link to={"/User"}> <button className="button">Back to home</button></Link>
-            </div>
-		</div>
-    )
+      <div id="table-responsive">
+        <table>
+          <thead id="session">
+            <tr>
+              <th><h3><b>Question</b></h3></th>
+              <th><h3><b>Answer</b></h3></th>
+            </tr>
+          </thead>
+          <tbody id="session">
+            {data.slice(0, data.length).map((item, index) => {
+              return (
+                <tr>
+                  <td><h5>{item[0]}</h5></td>
+                  <td><h5>{item[1]}</h5></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container"> 
+      <div>
+        <h2> Your Answers </h2>
+      </div>
+      <header className="jumbotron" id="session">
+        {table()}
+      </header>
+      <div className="buttons">
+        <Link to={"/User"}> <button className="button">Back to home</button></Link>
+      </div>
+    </div>
+  )
 }
 
 export default ViewSession;
