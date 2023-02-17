0434c07e3df171f6cbf8c1107f56623245b0014e
Mark Deligiannis <el19023@mail.ntua.gr>
2023-02-17 13:47:00 +0200

    CORS is no more
    -- You heard that right
    
    Co-Authored-By: Konstantinos Fotopoulos <117359782+ntua-el19196@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 9103;
+const frontend_server = 'http://localhost:3000';
 const baseRoute = "/intelliq_api"
 const mariadb = require('mariadb');
 
@@ -12,6 +13,15 @@ module.exports = pool
 
 const questionnaire_admin = require('./routes/admin.js');
 const questionnaire_user = require('./routes/user.js');
+
+// Add headers before the routes are defined (FIX CORS issue)
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', frontend_server);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(`${baseRoute}/admin/`, questionnaire_admin);
 app.use(`${baseRoute}/`, questionnaire_user);
